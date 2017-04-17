cd33a00816ca9967ac83e6f9c09560a47a684cf9
twinp <twinp@132.238.209.100>
2017-04-17 12:17:43 -0400

    closed scanner

--- src/com/mycompany/ass/chapter2/Program_2_15.java
+++ src/com/mycompany/ass/chapter2/Program_2_15.java
@@ -28,6 +28,7 @@ public class Program_2_15 {
 		System.out.printf("Subtract: %d%n", num1-num2);
 		System.out.printf("Multiply: %d%n", num1*num2);
 		System.out.printf("Divide: %d%n", num1/num2);
+		scan.close();
 		
 	}
 
